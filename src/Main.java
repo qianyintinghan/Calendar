af3d9c1ef8379b99bee29922a04bb0aa263e29a3
iLern <416138794@qq.com>
2019-04-23 11:06:59 +0800

    add weeks

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.FlowLayout;
 
 import javax.swing.*;
 
 public class Main {
-	static public void main(String args[]) {
-		JFrame frame = new JFrame();
-		frame.setTitle("Calendar");
-		frame.setLocation(500, 300);
-		frame.setSize(500, 500);
-//		frame.pack();
-//		frame.setVisible(true);
-		
+	static void addYearAndMonth(JFrame f) {
 		JPanel top = new JPanel();
 		top.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 5));
 		
@@ -32,7 +26,31 @@ public class Main {
 		top.add(yearP);
 		top.add(monthP);
 		
-		frame.add(top, BorderLayout.NORTH);
+		f.add(top, BorderLayout.NORTH);
+	}
+	
+	static void addWeek(JFrame f) {
+		JPanel body = new JPanel();
+		
+		body.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 20));
+		
+		String[] weeks = new String[] {"Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"};
+		for (int i = 0; i < 7; i++) {
+			body.add(new Button(weeks[i]));
+		}
+		
+		f.add(body);
+	}
+	
+	static public void main(String args[]) {
+		JFrame frame = new JFrame();
+		frame.setTitle("Calendar");
+		frame.setLocation(500, 300);
+		frame.setSize(500, 500);
+		
+		addYearAndMonth(frame);
+		addWeek(frame);
+		
 		
 //		frame.pack();
 		frame.setVisible(true);
