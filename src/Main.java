c4d05f8a00b93b58836b5d5016f80af5abc36691
iLern <416138794@qq.com>
2019-04-23 10:45:55 +0800

    Add year and month chooser

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
-import java.awt.Font;
-import java.util.concurrent.*;
+import javax.swing.*;
 
 public class Main {
 	static public void main(String args[]) {
-		JFrame frame = new JFrame("Calender");
-		JLabel label = new JLabel("A lebal");
+		JFrame frame = new JFrame();
+		frame.setTitle("Calendar");
+		frame.setLocation(500, 300);
+		frame.setSize(500, 500);
+//		frame.pack();
+//		frame.setVisible(true);
 		
-		frame.add(label);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600, 600);
+		JPanel top = new JPanel();
+		top.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 5));
+		
+		JPanel yearP = new JPanel();
+		String[] years = new String[] {"2017", "2018", "2019", "2020"};
+		JLabel year = new JLabel("年");
+		JComboBox<String> yearChooser = new JComboBox<>(years);
+		yearP.add(yearChooser);
+		yearP.add(year);
+		
+		JPanel monthP = new JPanel();
+		String[] months = new String[] {"一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"};
+		JLabel month = new JLabel("月");
+		JComboBox<String> monthChooser = new JComboBox<>(months);
+		monthP.add(monthChooser);
+		monthP.add(month);
+		
+		top.add(yearP);
+		top.add(monthP);
+		
+		frame.add(top, BorderLayout.NORTH);
+		
+//		frame.pack();
 		frame.setVisible(true);
-		
-		label.setFont(new Font("Consolas", 100, 100));
 	}
 }
